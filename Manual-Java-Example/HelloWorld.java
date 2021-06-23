8b7004924afde6190537871221a002a32e6a8110
jvalencia4 <johanavalencia53@gmail.com>
2021-06-23 14:22:59 -0600

    HelloWorld

---
+++ Manual-Java-Example/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+        //This is a comment
+        /*
+        This is a multi-line comment.
+        comments are notes in our program and they do not affect how program runs.
+        */
+        //Between the {} after the class declaration
+        //defines the body of the class
+        //THE MAIN Method!! This is special in Java
+        //Always the starting point of the program
+        public static void main(String[] args){
+            System.out.println("Hello World!");
+        }
+    }
